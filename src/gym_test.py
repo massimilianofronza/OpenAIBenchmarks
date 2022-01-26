8f4bc9e636df17a0da724474c56a58e9139426e4
massimilianofronza <maxfronza97@gmail.com>
2022-01-26 09:44:27 +0100

    Add config file and modifications

--- src/gym_test.py
+++ src/gym_test.py
@@ -1,7 +1,6 @@
 import gym
 #import neat
 
-
 # Classic control
 #env = gym.make('CartPole-v1')
 #env = gym.make('MountainCar-v0')
@@ -16,26 +15,33 @@ env = gym.make('LunarLander-v2')
 #env = gym.make('BipedalWalkerHardcore-v3')
 #env = gym.make('CarRacing-v0')
 
+config_files = ['/config/config_ffnn_lander.txt']
+
 for episode in range(5):
     observation = env.reset()
 
     for t in range(100):
+        
+        # Comment for faster learning
         env.render()
-        #print(observation)
-        action = 3#env.action_space.sample() # or given a custom model, action = policy(observation)
-        print(action)
-        # action = 0 moves cart to the left
-        # action = 1 moves cart to the right
+
+        print("Obs[2, 3, 4, 5, 6, 7]:\t", observation[2], "\t", observation[3], "\t", observation[4],
+             "\t", observation[5], "\t\t", observation[6], " - ", observation[7])
+        
+        action = env.action_space.sample() # or given a custom model, action = policy(observation)
         observation, reward, done, info = env.step(action)
 
         if done:
-            print("Episode finished after {} timesteps".format(t+1), "\n")
+            print("\nEpisode finished after {} timesteps".format(t+1))
+            print("Final reward:", reward)
             break
+    
+    print("end of episode.\n")
 
 # Useful information
-print("Action space: ", env.action_space)
-print("Observation space: ", env.observation_space)
-print("\thigh: ", env.observation_space.high)
-print("\tlow: ", env.observation_space.low)
+#print("Action space: ", env.action_space)
+#print("Observation space: ", env.observation_space)
+#print("\thigh: ", env.observation_space.high)
+#print("\tlow: ", env.observation_space.low)
 
 env.close()
