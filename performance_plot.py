2e7cf65d8879d62ed1bdd10699e6b96dbca98d4a
massimilianofronza <maxfronza97@gmail.com>
2022-02-13 19:22:20 +0100

    Add multiple genomes boxplot

--- performance_plot.py
+++ performance_plot.py
@@ -4,22 +4,19 @@ import os
 import pickle
 import pylab as pl
 
-from CartPole import Genome_C
 from LunarLander import Genome_L
 
 
-ENVIRONMENTS = ["CartPole", "LunarLander"]
-CONFIG_PATHS = [
-    "./LunarLander/singleGenomes/min_fitness/config_ffnn_lunarlander",
-    "./LunarLander/singleGenomes/nice/config_ffnn_lunarlander"
-]
-RESULTS_FOLDER = [
+RESULTS = [
     "./LunarLander/singleGenomes/min_fitness/",
-    "./LunarLander/singleGenomes/nice/"
+    "./LunarLander/singleGenomes/no_hidden/",
+    "./LunarLander/singleGenomes/mean_fitness/",
+    "./LunarLander/singleGenomes/no_elitism/",
+    "./LunarLander/singleGenomes/two_hidden/"
 ]
 
-ENV_IDS = [0, 1]        # Insert the genome ids that you want to plot
-NUM_SIMULATIONS = 100
+ENV_IDS = [0, 1, 2, 3, 4]        # Insert the genome ids that you want to plot
+NUM_SIMULATIONS = 5000
 NUM_CORES = 8
 
 
@@ -28,21 +25,25 @@ NUM_CORES = 8
 if __name__ == '__main__':
     
     individual = None
+    
     min_scores = []
-    nice_scores = []
-    CustomGenome = Genome_L.LanderGenome
+    no_hidden_scores = []
+    mean_scores = []
+    no_elitism_scores = []
+    two_hidden_scores = []
+    
     local_dir = os.path.dirname(__file__)
 
     # Start gathering scores from all the saved genomes
     for ID in ENV_IDS:
         
         # Obtain the winner
-        with open(RESULTS_FOLDER[ID] + 'winner.pickle', 'rb') as file:
+        with open(RESULTS[ID] + 'winner.pickle', 'rb') as file:
             individual = pickle.load(file)
             print("\nWinner: ", individual, "\n")
 
-        config_file = os.path.join(local_dir, CONFIG_PATHS[ID])
-        config = neat.Config(CustomGenome, neat.DefaultReproduction,
+        config_file = os.path.join(local_dir, RESULTS[ID] + "config_ffnn_lunarlander")
+        config = neat.Config(Genome_L.LanderGenome, neat.DefaultReproduction,
                             neat.DefaultSpeciesSet, neat.DefaultStagnation,
                             config_file)
         
@@ -65,25 +66,35 @@ if __name__ == '__main__':
             if ID==0:
                 min_scores.append(newScore)
             elif ID==1:
-                nice_scores.append(newScore)
+                no_hidden_scores.append(newScore)
+            elif ID==2:
+                mean_scores.append(newScore)
+            elif ID==3:
+                no_elitism_scores.append(newScore)
+            elif ID==4:
+                two_hidden_scores.append(newScore)
             else:
                 print(">>>>>>>> You added an ENV_ID that's not associated <<<<<<<<")
 
 
-    nice_scores.sort()
     min_scores.sort()
+    no_hidden_scores.sort()
+    mean_scores.sort()
+    no_elitism_scores.sort()
+    two_hidden_scores.sort()
 
     # Boxplot creation
     fig = pl.figure('Genomes (best fitnesses)')
     ax = fig.gca()
-    ax.boxplot([nice_scores, min_scores], notch=False)
-    ax.set_xticklabels(['Nice genome', 'Min genome'])
+    ax.boxplot([min_scores, no_hidden_scores, mean_scores, no_elitism_scores, two_hidden_scores], notch=False)
+    ax.set_xticklabels(['Min fitness', 'No hidden nodes', 'Mean fitness', 'No elitism', 'Two hidden nodes'])
     #ax.set_yscale('log')
     ax.set_xlabel('Best obtained genomes')
     ax.set_ylabel('Best fitness')
     pl.show()
 
     print("Values:", 
-        "\n\tNice:\n\tHighest: ", nice_scores[-1], "\tLowest: ", nice_scores[0], len(nice_scores),
-        "\n\tMin:\n\tHighest: ", min_scores[-1], "\tLowest: ", min_scores[0], len(min_scores))
+        "\n\tMin:\n\tHighest: ", min_scores[-1], "\tLowest: ", min_scores[0], len(min_scores), min_scores,
+        "\n\tNo hidden:\n\tHighest: ", no_hidden_scores[-1], "\tLowest: ", no_hidden_scores[0], len(no_hidden_scores), no_hidden_scores
+    )
 
